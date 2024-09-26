f8e0c9fe426a7c6ae147f5244ebfa320dcb61d53
Kate Stingle <35676400+katestingle@users.noreply.github.com>
2024-09-25 23:45:07 -0700

    First Coding Day Notes 9/25/2024
    First few programs in C++ - getting comfy with it (:

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+//This program demonstrates simple output to the screen.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!!" << endl;
+	return 0;
+}
---
+++ InputOutputTest.cpp
@@ -0,0 +1,22 @@
+//C++ is a big library of files
+#include <iostream>
+//import input output stream from terminal, essentially
+
+using namespace std;
+//std is a namespace
+
+
+int main() {
+	//entry point for C++, public static void main(String[] Args){} from Java
+	std::cout << "Hello World!"; //parenthesis "" tells C++ this is not code, it is a String, it is text
+	//std is not required as using namespace std is included earlier in file
+
+	//what is :: ????????
+	// 
+	//cout is a function that lives somewhere else in the computer
+	//cout is a function from iostream
+
+	//what is << ????????
+
+	return 0; //C++ ends with terminal return of 0, end file
+}
